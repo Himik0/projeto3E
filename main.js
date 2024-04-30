1efa9e17ec809ab66ee79ddea380e5f7fc1f5157
Liss <107251720+Himik0@users.noreply.github.com>
2024-04-30 07:47:26 -0300

    a

--- main.js
+++ main.js
@@ -20,4 +20,22 @@ let tempoAtual = new Date();
 for(let i=0; i< contadores.length; i++){
     contadores[i].textContent = calcularTempo(Tempo[i]);
 }
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calcularTempo(tempoObjetivo1);
+
+function calcularTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoAtual = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempofinal / 1000)
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias"
+    + horas + "horas"
+    + minutos + "minutos"
+    + segundos + "segundos";
+}
